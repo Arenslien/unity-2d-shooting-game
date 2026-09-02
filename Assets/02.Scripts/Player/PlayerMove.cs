205c066294e018f4ccec9cbc98eedf50947f234b
Sunghoon Jung <46517668+Arenslien@users.noreply.github.com>
2026-09-02 13:54:09 +0900

    refactor: 기능 메서드로 분리

--- Assets/02.Scripts/Player/PlayerMove.cs
+++ Assets/02.Scripts/Player/PlayerMove.cs
@@ -23,35 +23,22 @@ public class PlayerMove : MonoBehaviour
     // 초당 프레임 실행 횟수는: 별다른 설정이 없을 경우 가능한 많이 실행
     private void Update()
     {
+        HandleSpeed();
+        Move();
+    }
+
+    private void Move()
+    {
         // [기본적인 이동 메커니즘]
         // 1. 키보드 입력을 받는다.
-        float h = Input.GetAxisRaw("Horizontal"); // 키보드 좌우 입력 상태에 따라 -1f ~ 0 ~ 1f
-        float v = Input.GetAxisRaw("Vertical");  // 키보드 상하 입력 상태에 따라 -1f ~ 0 ~ 1f
+        float h = Input.GetAxisRaw("Horizontal"); // 키보드 좌우 입력 상태에 따라 -1f, 0, 1f(Raw 빼면 연속으로)
+        float v = Input.GetAxisRaw("Vertical");   // 키보드 상하 입력 상태에 따라 -1f, 0, 1f
         // Debug.Log($"h: {h}, v: {v}");
         
         // 2. 키보드 입력에 따라 방향을 구한다.
-        // 게임에는 벡터라는 타입이 있다. (벡터: 크기와 방향)
-        Vector2 direction = new Vector2(h, v); 
-        // Vector2 direction = Vector2.left; // = Vector2(-1, 0) : 내부적으로 같은 코드
+        Vector2 direction = new Vector2(h, v); // 게임에는 벡터라는 타입이 있다. (벡터: 크기와 방향) 
         
         // 3. 방향과 속력에 따라 이동한다.
-        // 속도 = 방향 + 속력
-        // 매직넘버: 보는 사람에 따라 의미가 달라지 수 있는 헷갈리는 숫자
-        // transform.Translate(direction * Speed * Time.deltaTime);
-        // deltaTime: 이전 프레임으로부터 지금 프레임까지 시간이 얼마나 지났는지 ms(1/1000)로 반환
-
-        // 3. 키보드 E, Q에 스피드 업, 다운 기능 구현
-        if (Input.GetKey(KeyCode.E))
-        {
-            Speed += 1.0f * Time.deltaTime;
-            Debug.Log($"현재 속도: {Speed}");
-        }
-        if (Input.GetKey(KeyCode.Q))
-        {
-            Speed -= 1.0f * Time.deltaTime;
-            Debug.Log($"현재 속도: {Speed}");
-        }
-        
         Vector2 normalizedSpeed = direction.normalized * Speed; // 벡터의 길이 1로 변환. 즉, 방향만 유지
         
         // 새로운 위치 = 현재 위치 + v(방향 * 속력) x t(시간)
@@ -71,5 +58,20 @@ public class PlayerMove : MonoBehaviour
         }
         
         transform.position = newPosition;
+    }
+
+    private void HandleSpeed()
+    {
+        // 3. 키보드 E, Q에 스피드 업, 다운 기능 구현
+        if (Input.GetKey(KeyCode.E))
+        {
+            Speed += 1.0f * Time.deltaTime;
+            Debug.Log($"현재 속도: {Speed}");
+        }
+        if (Input.GetKey(KeyCode.Q))
+        {
+            Speed -= 1.0f * Time.deltaTime;
+            Debug.Log($"현재 속도: {Speed}");
+        }
     }
 }
