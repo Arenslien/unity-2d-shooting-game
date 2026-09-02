22f4aea8c383e158fe1f21f6b7cd72ffd6dad423
Sunghoon Jung <46517668+Arenslien@users.noreply.github.com>
2026-09-02 12:36:39 +0900

    feat: 영역 내 이동 기능 구현 (좌우 제외)

--- Assets/02.Scripts/Player/PlayerMove.cs
+++ Assets/02.Scripts/Player/PlayerMove.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System;
 
 public class PlayerMove : MonoBehaviour
 {
@@ -6,6 +7,16 @@ public class PlayerMove : MonoBehaviour
     
     // 필요 필드:
     public float Speed;
+    public float MinY = -4.6f;
+    public float MaxY = -0.58f;
+    public float LimitX = 2.9f;
+    public float WarpX = 1.85f;
+
+    private void Start()
+    {
+        transform.position = new Vector3(0, -2.58f, 0);
+    }
+    
     
     // Update 메서드는 매 프레임마다 실행
     // 초당 프레임 실행 횟수는: 별다른 설정이 없을 경우 가능한 많이 실행
@@ -20,18 +31,27 @@ public class PlayerMove : MonoBehaviour
         
         // 2. 키보드 입력에 따라 방향을 구한다.
         // 게임에는 벡터라는 타입이 있다. (벡터: 크기와 방향)
-        Vector2 direction = new Vector2(h, v); // 왼쪽 방향
-        // Vector2 direction = Vector2.left; // 내부적으로 같은 코드
+        Vector2 direction = new Vector2(h, v); 
+        // Vector2 direction = Vector2.left; // = Vector2(-1, 0) : 내부적으로 같은 코드
         
         // 3. 방향과 속력에 따라 이동한다.
         // 속도 = 방향 + 속력
         // 매직넘버: 보는 사람에 따라 의미가 달라지 수 있는 헷갈리는 숫자
         // transform.Translate(direction * Speed * Time.deltaTime);
         // deltaTime: 이전 프레임으로부터 지금 프레임까지 시간이 얼마나 지났는지 ms(1/1000)로 반환
-        
+
+        Vector2 normalizedSpeed = direction.normalized * Speed; // 벡터의 길이 1로 변환. 즉, 방향만 유지
         // 새로운 위치 = 현재 위치 + v(방향 * 속력) x t(시간)
-        Vector2 normalizedSpeed = direction.normalized * Speed; // 벡터의 길이 1로 변환
-                                                        // 즉, 방향만 유지
         transform.position += (Vector3)normalizedSpeed * Time.deltaTime;
+        
+        // 1. 영역 안에서 움직이도록 고정
+        if (transform.position.y < MinY)
+        {
+            transform.position = new Vector3(transform.position.x, MinY, 0);
+        }
+        if (transform.position.y > MaxY)
+        {
+            transform.position = new Vector3(transform.position.x, MaxY, 0);
+        }
     }
 }
