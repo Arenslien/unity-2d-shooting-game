4edd31615ca8f9b7fba790672660bb1496da919e
Sunghoon Jung <46517668+Arenslien@users.noreply.github.com>
2026-09-02 11:02:48 +0900

    feat: 플레이어 이동 기능 개선

--- Assets/02.Scripts/Player/PlayerMove.cs
+++ Assets/02.Scripts/Player/PlayerMove.cs
@@ -14,23 +14,24 @@ public class PlayerMove : MonoBehaviour
     {
         // [기본적인 이동 메커니즘]
         // 1. 키보드 입력을 받는다.
+        float h = Input.GetAxis("Horizontal"); // 키보드 좌우 입력 상태에 따라 -1f ~ 0 ~ 1f
+        float v = Input.GetAxis("Vertical");  // 키보드 상하 입력 상태에 따라 -1f ~ 0 ~ 1f
         
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            Debug.Log("왼쪽 방향키를 누르는 중");
-            
-            // 2. 키보드 입력에 따라 방향을 구한다.
-            // 게임에는 벡터라는 타입이 있다. (벡터: 크기와 방향)
-            Vector2 direction = new Vector2(-1, 0); // 왼쪽 방향
-            // Vector2 direction = Vector2.left; // 내부적으로 같은 코드
-            
-            // 3. 방향과 속력에 따라 이동한다.
-            // 속도 = 방향 + 속력
-            // 매직넘버: 보는 사람에 따라 의미가 달라지 수 있는 헷갈리는 숫자
-            transform.Translate(direction * Speed * Time.deltaTime);
-            // deltaTime: 이전 프레임으로부터 지금 프레임까지 시간이 얼마나 지났는지 ms(1/1000)로 반환
-        }
+        Debug.Log($"h: {h}, v: {v}");
         
+
+        // 2. 키보드 입력에 따라 방향을 구한다.
+        // 게임에는 벡터라는 타입이 있다. (벡터: 크기와 방향)
+        Vector2 direction = new Vector2(h, v); // 왼쪽 방향
+        // Vector2 direction = Vector2.left; // 내부적으로 같은 코드
         
+        // 3. 방향과 속력에 따라 이동한다.
+        // 속도 = 방향 + 속력
+        // 매직넘버: 보는 사람에 따라 의미가 달라지 수 있는 헷갈리는 숫자
+        // transform.Translate(direction * Speed * Time.deltaTime);
+        // deltaTime: 이전 프레임으로부터 지금 프레임까지 시간이 얼마나 지났는지 ms(1/1000)로 반환
+        
+        // 새로운 위치 = 현재 위치 + v(방향 * 속력) x t(시간)
+        transform.position += (Vector3)direction * Speed * Time.deltaTime;
     }
 }
