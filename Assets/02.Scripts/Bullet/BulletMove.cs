6fa6ed6be76ed7b1bf55d13e1a4c93fc7df88fed
Sunghoon Jung <46517668+Arenslien@users.noreply.github.com>
2026-09-02 14:23:17 +0900

    feat: bullet 오브젝트 생성 및 이동 기능 구현

---
+++ Assets/02.Scripts/Bullet/BulletMove.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class BulletMove : MonoBehaviour
+{
+    public float Speed;
+
+    private void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        // 2. 방향 설정
+        Vector2 direction = Vector2.up; // new Vector2(0, 1)과 동일
+            
+        // 3. 발사
+        transform.Translate(direction * Speed * Time.deltaTime);
+
+        if (transform.position.y > 5.2)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
