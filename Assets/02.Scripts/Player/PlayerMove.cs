959bd588522bb2ae2467e7c64f595b4f2a1d6a5d
Sunghoon Jung <46517668+Arenslien@users.noreply.github.com>
2026-09-02 10:55:40 +0900

    feat: 기본 방향 설정 및 이동 구현

--- Assets/02.Scripts/Player/PlayerMove.cs
+++ Assets/02.Scripts/Player/PlayerMove.cs
@@ -3,16 +3,34 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour
 {
     // 목적: 키보드 입력에 따라서 플레이어 이동 처리를 하고 싶다.
+    
+    // 필요 필드:
+    public float Speed;
+    
+    // Update 메서드는 매 프레임마다 실행
+    // 초당 프레임 실행 횟수는: 별다른 설정이 없을 경우 가능한 많이 실행
+    
     private void Update()
     {
         // [기본적인 이동 메커니즘]
         // 1. 키보드 입력을 받는다.
+        
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             Debug.Log("왼쪽 방향키를 누르는 중");
+            
+            // 2. 키보드 입력에 따라 방향을 구한다.
+            // 게임에는 벡터라는 타입이 있다. (벡터: 크기와 방향)
+            Vector2 direction = new Vector2(-1, 0); // 왼쪽 방향
+            // Vector2 direction = Vector2.left; // 내부적으로 같은 코드
+            
+            // 3. 방향과 속력에 따라 이동한다.
+            // 속도 = 방향 + 속력
+            // 매직넘버: 보는 사람에 따라 의미가 달라지 수 있는 헷갈리는 숫자
+            transform.Translate(direction * Speed * Time.deltaTime);
+            // deltaTime: 이전 프레임으로부터 지금 프레임까지 시간이 얼마나 지났는지 ms(1/1000)로 반환
         }
         
-        // 2. 키보드 입력에 따라 방향을 구한다.
-        // 3. 방향과 속도에 따라 이동한다.
+        
     }
 }
