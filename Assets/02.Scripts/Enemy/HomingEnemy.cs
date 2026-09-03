cd0b09e3def0351ac94d593f553c6eac04991e51
Sunghoon Jung <46517668+Arenslien@users.noreply.github.com>
2026-09-03 14:29:55 +0900

    feat: HomingEnemy 구현 완료
    - 잘 따라오는구만
    - GameObject.FindWithTag("Player") 활용

---
+++ Assets/02.Scripts/Enemy/HomingEnemy.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class HomingEnemy : Enemy
+{
+    protected override void Move()
+    {
+        GameObject playerObject = GameObject.FindWithTag("Player");
+
+        Vector2 direction = (playerObject.transform.position - transform.position).normalized;
+
+        transform.Translate(direction * (MoveSpeed * Time.deltaTime));
+    }
+}
