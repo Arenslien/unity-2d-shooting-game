f8dc6e15650bafc5f848752cd3df6bfb3c1cbcf7
Sunghoon Jung <46517668+Arenslien@users.noreply.github.com>
2026-09-03 11:09:30 +0900

    refactor: 매직 넘버 개선

--- Assets/02.Scripts/Enemy/Enemy.cs
+++ Assets/02.Scripts/Enemy/Enemy.cs
@@ -3,13 +3,14 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     public float Speed;
+    private float _minPositionY = -5.5f;
 
     private void Update()
     {
         Move();
 
         // 3. 오브젝트 삭제
-        if (transform.position.y <= -5.5f)
+        if (transform.position.y <= _minPositionY)
         {
             Destroy(gameObject);
         }
@@ -21,6 +22,6 @@ public class Enemy : MonoBehaviour
         Vector2 direction = new Vector2(0, -1); // Vector2.down;
 
         // 2. 오브젝트 이동
-        transform.Translate(direction * Speed * Time.deltaTime);
+        transform.Translate(direction * (Speed * Time.deltaTime));
     }
 }
