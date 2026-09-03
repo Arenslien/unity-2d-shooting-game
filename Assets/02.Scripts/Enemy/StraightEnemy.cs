544d639f625377e5f6bd35112bc1f449d0165493
Sunghoon Jung <46517668+Arenslien@users.noreply.github.com>
2026-09-03 13:50:15 +0900

    feat: StraightEnemy 구현

---
+++ Assets/02.Scripts/Enemy/StraightEnemy.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class StraightEnemy : Enemy
+{
+    protected override void Move()
+    {
+        // 1. 방향 설정
+        Vector2 direction = new Vector2(0, -1); // Vector2.down;
+
+        // 2. 오브젝트 이동
+        transform.Translate(direction * (MoveSpeed * Time.deltaTime));
+    }
+}
