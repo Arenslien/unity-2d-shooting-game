ba2605f32ba2233791a65c875d74b57049debd4a
Sunghoon Jung <46517668+Arenslien@users.noreply.github.com>
2026-09-04 10:27:04 +0900

    chore: 테스트를 위한 FindwithTg 코드 위치 변경

--- Assets/02.Scripts/Enemy/HomingEnemy.cs
+++ Assets/02.Scripts/Enemy/HomingEnemy.cs
@@ -2,10 +2,13 @@ using UnityEngine;
 
 public class HomingEnemy : Enemy
 {
-    protected override void Move()
+    private void Update()
     {
         GameObject playerObject = GameObject.FindWithTag("Player");
+    }
 
+    protected override void Move()
+    {
         Vector2 direction = (playerObject.transform.position - transform.position).normalized;
 
         transform.Translate(direction * (_moveSpeed * Time.deltaTime));
