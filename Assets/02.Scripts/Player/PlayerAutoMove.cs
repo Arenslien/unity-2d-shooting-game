c98bf5fdbd67ed71f62cd8b88f81e5491eeb6c0c
Sunghoon Jung <46517668+Arenslien@users.noreply.github.com>
2026-09-10 10:17:06 +0900

    feat: 플레이어 자동 이동 뼈대 코드 구축

--- Assets/02.Scripts/Player/PlayerAutoMove.cs
+++ Assets/02.Scripts/Player/PlayerAutoMove.cs
@@ -1,11 +1,44 @@
 using UnityEngine;
 
+enum AutoModeState
+{
+    Idle,
+    Patrol,
+    Chase,
+}
+
 public class PlayerAutoMove : MonoBehaviour
 {
     private bool _isAutoMode = false;
+    private AutoModeState _autoModeState = AutoModeState.Idle;
 
     private void Update()
     {
-        if (Input.GetKeyDown())
+        ChangeAutoMode();
+
+        if (_isAutoMode) AutoMove();
+    }
+
+    private void ChangeAutoMode()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            _isAutoMode = !_isAutoMode;
+            _autoModeState = _isAutoMode ? AutoModeState.Patrol : AutoModeState.Idle;
+            Debug.Log($"[Player Auto Move] - {(_isAutoMode ? "ON" : "OFF")}");
+        }
+    }
+
+    private void AutoMove()
+    {
+        switch (_autoModeState)
+        {
+            case AutoModeState.Patrol:
+                // 1. 좌우 + 상하 패트롤
+                break;
+            case AutoModeState.Chase:
+                // 2. 가까이 가서 싸우는 게 아니라 흠..
+                break;
+        }
     }
 }
